368b40e58062d15dde85c6aa6f057900b93c6c4f
Mandy Vogel <mandy.vogel@googlemail.com>
2015-07-06 09:40:58 +0200

    rstudio beispiele v0.99

---
+++ session5/rstudiob099.r
@@ -0,0 +1,8 @@
+lm(mpg ~ cyl, data = mtcars))
+
+head(mtcars)
+
+require(dplyr)
+mtcars %>% group_by(cyl) %>% summarise(mpg = mean(mpg),
+                                       disp = mean(disp),
+                                       )
