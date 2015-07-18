f6935afb80c645d6f3fe01240a60a5d60daf2e5c
Mandy Vogel <mandy.vogel@googlemail.com>
2015-07-18 21:16:40 +0200

    add exercises and code file to session7mpi

--- session7mpi/session7mpi.r
+++ session7mpi/session7mpi.r
@@ -3,22 +3,29 @@ data(sleep)
 ## use of tapply
 tapply(sleep$extra,sleep$group,FUN = mean)
 
+## use of replicate
+## roll 6 dice
+sample(1:6,size = 1)
+## roll 6 dice 10000 times 
+dice <- replicate(10000,sum(sample(1:6,size = 6,replace = T)))
+table(dice)
+
+### Permutation tests
 ## calculate the difference between the group means
-orig.diff <- diff(tapply(sleep$extra,sleep$group,FUN = mean))
+diff(tapply(sleep$extra,sleep$group,FUN = mean))
 
 ## calculate the absolute difference (two-sided test)
-abs(diff(tapply(sleep$extra,sleep$group,FUN = mean)))
+orig.diff <- abs(diff(tapply(sleep$extra,sleep$group,FUN = mean)))
 
-## sample
-sample(sleep$group, size = length(sleep$group), replace = F)
-
-## do it a 1000 times
-res <- replicate(1000, abs(diff(tapply(sleep$extra,sample(sleep$group),FUN = mean))))
-
-sum(orig.diff < res)/1000
-
+## do it a 10000 times
+res <- replicate(10000, abs(diff(tapply(sleep$extra,sample(sleep$group),mean))))
+sum(orig.diff <= res)/10000 ## p-value
 
 ## compare with t-test
-t.test(extra ~ group, data = sleep)
-
 t.test(sleep$extra ~ sleep$group)
+
+### Rank Based Permutation tests
+
+### Exercises
+require(dplyr)
+babies <- filter(babies, smoke == 1 & gestation != 999)
