32168b85548fea20a232c8fd6a89aa6cd3604d02
Mandy Vogel <mandy.vogel@googlemail.com>
2015-07-28 07:54:43 +0200

    minor changes to session8exersol.r

--- session8aov/session8exersol.r
+++ session8aov/session8exersol.r
@@ -5,20 +5,20 @@ setwd("/media/mandy/Samsung_T1/201505kurs/session8aov/")
 
 x <- data.frame(id=1:3,score=rnorm(3))
 y <- data.frame(id=13:15,score=rnorm(3))
-rbind(x,y)
+z <- rbind(x,y)
 
 
 ## cbind
 x <- data.frame(id=1:5,score1=rnorm(5))
 y <- data.frame(score2=rnorm(5),score3=rnorm(5))
-cbind(x,y)
+z <- cbind(x,y)
 
 
 ## merge
 (d1 <- data.frame(id=LETTERS[c(1,2,3)],day1=sample(10,3)))
 (d2 <- data.frame(id=LETTERS[c(1,3,5,6)],day2=sample(10,4)))
 
-merge(d1,d2)
+merge(d1,d2,by="id")
 
 merge(d1,d2,all.x = T)
 
@@ -39,6 +39,8 @@ bp <- read_excel("nhanes1112.xlsx","bp")
 physact <- read_excel("nhanes1112.xlsx","physactivity")
 bm <- read_excel("nhanes1112.xlsx","bodymeas")
 
+## demogr <- read_excel("nhanes1112.xlsx",1)
+
 data <- merge(demogr,bp,by = "seqn")
 data <- merge(data,bm,by.x = "seqn", by.y = "seqn")
 data <- merge(data,physact)
@@ -47,3 +49,11 @@ data <- merge(data,physact)
 data <- merge(demogr,bp,by = "seqn", all = T)
 data <- merge(data,bm,by.x = "seqn", by.y = "seqn", all = T)
 data <- merge(data,physact, all = T)
+
+
+require(granovaGG)
+data(arousal)
+
+data.long <- stack(arousal)
+
+granovagg.1w(data.long$values,data.long$ind)
